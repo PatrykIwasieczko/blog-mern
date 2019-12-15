b134124550d9fd5b32c6202cfe630002e6455801
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-15 23:11:48 +0100

    Add delete a post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const Post = require("../../models/Post.js");
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get("/", (req, res) => {
+    Post.find().then(posts => res.json(posts));
+});
+
 // @route   POST api/posts
 // @desc    Post the post
 // @access  Public
@@ -14,6 +21,19 @@ router.post("/", (req, res) => {
     });
 
     newPost.save().then(item => res.json(item));
+});
+
+// @route   DELETE api/posts/id
+// @desc    Delete a post
+// @access  Public
+router.post("/:id", (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            post.remove().then(() =>
+                res.json({ msg: "Post successfully removed" })
+            );
+        })
+        .catch(err => res.status(404).json({ msg: "Post doesn't exist" }));
 });
 
 module.exports = router;
