7b7f490c494dcecd661a219791672126e8fc04a6
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-15 19:58:43 +0100

    Connect to mongo

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
 
 const app = express();
+
+// DB Config
+const db = config.get("mongoURI");
+
+// Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
