43df303afd5d0af20acbf2e305ddf89721aa4d0b
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-16 15:07:10 +0100

    Create navbar basic template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.scss";
 
+// Components
+import AppNavbar from "./components/AppNavbar";
+
 function App() {
     return (
         <div>
-            <h1>Hello world</h1>
+            <AppNavbar />
+            <div className="container">
+                <h1 className="content">Hello world</h1>
+            </div>
         </div>
     );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AppNavbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <a href="#">Posts</a>
+                </li>
+                <li>
+                    <a href="#">Posts</a>
+                </li>
+                <li>
+                    <a href="#">Posts</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">Posts</a>
+                </li>
+                <li>
+                    <a href="#">Developers</a>
+                </li>
+                <li>
+                    <a href="#">Register</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default AppNavbar;
