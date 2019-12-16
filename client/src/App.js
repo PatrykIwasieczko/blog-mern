ca5475c4deaa12a0e829cbac7c0b2af4c4d420b6
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-16 17:29:52 +0100

    Set up browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./App.scss";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 // Components
 import AppNavbar from "./components/AppNavbar";
 import Landing from "./components/Landing";
 import Posts from "./components/Posts";
+import Post from "./components/Post";
 
 function App() {
     return (
         <div>
             <AppNavbar />
             <Landing />
-            <Posts />
+            <Switch>
+                <Route exact path="/" component={Posts} />
+                <Route path="/post" component={Post} />
+                <Redirect to="/" />
+            </Switch>
         </div>
     );
 }
