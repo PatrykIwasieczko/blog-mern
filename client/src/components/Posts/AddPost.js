38dc4f19ade7418d0d1d2cf073b1508cc0636d46
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-18 19:33:16 +0100

    Add adding post functionality

--- client/src/components/Posts/AddPost.js
+++ client/src/components/Posts/AddPost.js
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class AddPost extends Component {
     state = {
-        authors: ["First author", "Second author"]
+        authors: ["First author", "Second author"],
+        post: {
+            author: "",
+            body: "",
+            title: ""
+        }
+    };
+
+    handlePostChange = event => {
+        this.setState({
+            post: {
+                ...this.state.post,
+                [event.target.name]: event.target.value
+            }
+        });
+    };
+
+    addPost = event => {
+        event.preventDefault();
+        const { author, body, title } = this.state.post;
+        const newPost = {
+            author,
+            body,
+            title
+        };
+        axios
+            .post("http://localhost:5000/api/posts", newPost)
+            .then(() => {
+                this.setState({
+                    post: {
+                        author: "",
+                        body: "",
+                        title: ""
+                    }
+                });
+            })
+            .then(() => {
+                this.props.history.push("/");
+            })
+            .catch(err => {
+                console.log(err);
+            });
     };
 
     render() {
@@ -11,17 +53,34 @@ class AddPost extends Component {
                 <h1>Add a Post</h1>
                 <form className="form">
                     <p>Post title</p>
-                    <input type="text" placeholder="Enter your name" />
+                    <input
+                        value={this.state.post.title}
+                        onChange={this.handlePostChange}
+                        type="text"
+                        name="title"
+                        placeholder="Enter post title"
+                    />
                     <p>Post content</p>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <textarea
+                        value={this.state.post.body}
+                        onChange={this.handlePostChange}
+                        name="body"
+                        id=""
+                        cols="30"
+                        rows="10"
+                    ></textarea>
                     <p>Author</p>
                     <div className="comment-grid">
-                        <select name="authors">
-                            {this.state.authors.map(author => (
-                                <option value={author}>{author}</option>
+                        <select onChange={this.handlePostChange} name="author">
+                            {this.state.authors.map((author, index) => (
+                                <option key={index} value={author}>
+                                    {author}
+                                </option>
                             ))}
                         </select>
-                        <button className="btn">Submit</button>
+                        <button onClick={this.addPost} className="btn">
+                            Submit
+                        </button>
                     </div>
                 </form>
             </div>
