527b0e5230a207dd98678a14af46140baa1a8f40
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-19 12:10:45 +0100

    Add spinner on main page posts loading

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,51 +2,38 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 
+// Components
+import Spinner from "../UI/Spinner";
+
 class Posts extends Component {
     state = {
-        posts: [
-            // {
-            //     title: "first",
-            //     body:
-            //         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, fugiat.",
-            //     author: "Patryk",
-            //     img: "/images/food1.jpg"
-            // },
-            // {
-            //     title: "second",
-            //     body:
-            //         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, fugiat.",
-            //     author: "Patryk",
-            //     img: "/images/food2.jpg"
-            // },
-            // {
-            //     title: "third",
-            //     body:
-            //         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, fugiat.",
-            //     author: "Patryk",
-            //     img: "/images/food3.jpg"
-            // },
-            // {
-            //     title: "fourth",
-            //     body:
-            //         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, fugiat.",
-            //     author: "Patryk",
-            //     img: "/images/food4.jpg"
-            // }
-        ]
+        posts: [],
+        loading: false
     };
 
     componentDidMount() {
+        this.setState({ loading: true });
         axios
             .get("http://localhost:5000/api/posts")
             .then(res => {
-                this.setState({ posts: res.data });
+                this.setState({ posts: res.data, loading: false });
             })
             .catch(err => console.error(err));
+        this.setState({ loading: false });
     }
     render() {
+        let postSpinner = this.state.loading ? (
+            <>
+                <Spinner />
+                <Spinner />
+                <Spinner />
+                <Spinner />
+            </>
+        ) : null;
+
         return (
             <div className="posts container">
+                {postSpinner}
                 {this.state.posts.map(post => (
                     <div key={post._id} className="post" postid={post._id}>
                         <img src="/images/food4.jpg" alt="" />
