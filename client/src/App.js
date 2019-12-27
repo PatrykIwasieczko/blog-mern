d50f79d3d90147799c9a705bee3e1f0fcbcaf3d4
Patryk Iwasieczko <patryk.iwasieczko@gmail.com>
2019-12-27 21:37:04 +0100

    Set up redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./App.scss";
 import { Route, Switch, Redirect } from "react-router-dom";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 // Components
 import AppNavbar from "./components/Layout/AppNavbar";
 import Landing from "./components/Layout/Landing";
@@ -14,19 +18,21 @@ import AllPosts from "./components/Posts/AllPosts";
 
 function App() {
     return (
-        <div>
-            <AppNavbar />
-            <Landing />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/addpost" component={AddPost} />
-                <Route path="/allposts" component={AllPosts} />
-                <Route path="/:id" component={FullPost} />
-                <Redirect to="/" />
-            </Switch>
-            <Footer />
-            <TopArrow />
-        </div>
+        <Provider store={store}>
+            <div>
+                <AppNavbar />
+                <Landing />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/addpost" component={AddPost} />
+                    <Route path="/allposts" component={AllPosts} />
+                    <Route path="/:id" component={FullPost} />
+                    <Redirect to="/" />
+                </Switch>
+                <Footer />
+                <TopArrow />
+            </div>
+        </Provider>
     );
 }
 
