f7b58574b2e3327b4a1811ce9beb5d92d023e204
Patryk <patryk.iwasieczko@gmail.com>
2020-01-22 21:54:21 +0100

    Add auth action types and set up auth action and reducer

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -4,3 +4,12 @@ export const ADD_POST = "ADD_POST";
 export const DELETE_POST = "DELETE_POST";
 export const POSTS_LOADING = "POSTS_LOADING";
 export const STOP_POSTS_LOADING = "STOP_POSTS_LOADING";
+
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
