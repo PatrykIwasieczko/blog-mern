509c9710fdbc47a3ad929229f7b5557e47c877aa
Patryk <patryk.iwasieczko@gmail.com>
2020-01-24 19:36:34 +0100

    Change appnavbar links when user is logged in or out

--- client/src/components/Layout/AppNavbar.js
+++ client/src/components/Layout/AppNavbar.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-const AppNavbar = () => {
-    return (
-        <nav className="navbar">
-            <ul>
+import { connect } from "react-redux";
+
+class AppNavbar extends Component {
+    render() {
+        const { isAuthenticated, user } = this.props.auth;
+        const userLinks = (
+            <>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
                 <li>
                     <NavLink to="/addpost">Add Post</NavLink>
                 </li>
@@ -15,28 +21,55 @@ const AppNavbar = () => {
                     <NavLink to="/about">About</NavLink>
                 </li>
                 <li>
+                    <NavLink to="/logout">Logout</NavLink>
+                </li>
+            </>
+        );
+
+        const guestLinks = (
+            <>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/allposts">All Posts</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
                     <NavLink to="/login">Login</NavLink>
                 </li>
-            </ul>
-            <ul>
-                <li>
-                    <NavLink to="/">
-                        <i className="fab fa-facebook fa-2x"></i>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">
-                        <i className="fab fa-instagram fa-2x"></i>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">
-                        <i className="fab fa-linkedin fa-2x"></i>
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
-};
+            </>
+        );
 
-export default AppNavbar;
+        return (
+            <nav className="navbar">
+                <ul>{isAuthenticated ? userLinks : guestLinks}</ul>
+                <ul>
+                    <li>
+                        <NavLink to="/">
+                            <i className="fab fa-facebook fa-2x"></i>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">
+                            <i className="fab fa-instagram fa-2x"></i>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">
+                            <i className="fab fa-linkedin fa-2x"></i>
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(AppNavbar);
