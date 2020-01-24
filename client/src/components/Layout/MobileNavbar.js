6f85af419f2804f2a68485efcaf129a529af768c
Patryk <patryk.iwasieczko@gmail.com>
2020-01-24 20:01:34 +0100

    Change mobileNavbar links when user is logged in or out

--- client/src/components/Layout/MobileNavbar.js
+++ client/src/components/Layout/MobileNavbar.js
@@ -1,49 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-const MobileNavbar = () => {
-    return (
-        <div className="menu-wrap">
-            <input type="checkbox" className="toggler" />
-            <div className="hamburger">
-                <div></div>
-            </div>
-            <div className="menu">
-                <div>
+import { connect } from "react-redux";
+
+class MobileNavbar extends Component {
+    render() {
+        const { isAuthenticated, user } = this.props.auth;
+        const userLinks = (
+            <>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/addpost">Add Post</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/allposts">All Posts</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/logout">Logout</NavLink>
+                </li>
+            </>
+        );
+
+        const guestLinks = (
+            <>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/allposts">All Posts</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login">Login</NavLink>
+                </li>
+            </>
+        );
+        return (
+            <div className="menu-wrap">
+                <input type="checkbox" className="toggler" />
+                <div className="hamburger">
+                    <div></div>
+                </div>
+                <div className="menu">
                     <div>
-                        <ul>
-                            <li>
-                                <NavLink to="/addpost">Add Post</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/allposts">All Posts</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/">Posts</NavLink>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <NavLink to="/">
-                                    <i className="fab fa-facebook fa-2x"></i>
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/">
-                                    <i className="fab fa-instagram fa-2x"></i>
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/">
-                                    <i className="fab fa-linkedin fa-2x"></i>
-                                </NavLink>
-                            </li>
-                        </ul>
+                        <div>
+                            <ul>{isAuthenticated ? userLinks : guestLinks}</ul>
+                            <ul>
+                                <li>
+                                    <NavLink to="/">
+                                        <i className="fab fa-facebook fa-2x"></i>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/">
+                                        <i className="fab fa-instagram fa-2x"></i>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/">
+                                        <i className="fab fa-linkedin fa-2x"></i>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
-export default MobileNavbar;
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(MobileNavbar);
